535570f0d51a3a55117c5c00072e14fd7852304c
Emre Hacıhassan <emrehacihassan01@gmail.com>
2025-03-29 16:01:06 +0300

    Add files via upload

---
+++ Angela_Yu_Day#1.py
@@ -0,0 +1,13 @@
+#Emre's Band Name Generator Project 
+x =   input("This is The Band Name Generator \nwhat is the name of your hometown ")     
+
+y = input("This is the Band Name Generator \nWhat is the name of your first pet ")
+
+#print(f"The name of the band is: {x } {y}" )
+
+print("The name of the band is: " +(x + " "+ y))
+
+
+#This project is from Bootcamp which Angela Yu made in Udemy and this project belong to first day ,I completed the project with two different way ,first way is f string and second way is using "+" instead of using f string
+
+
