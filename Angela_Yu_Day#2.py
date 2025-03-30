cc2fe88b4b28d587ac580ac025aa4a1a239a18fb
Emre Hacıhassan <emrehacihassan01@gmail.com>
2025-03-30 12:48:55 +0300

    Add files via upload

---
+++ Angela_Yu_Day#2.py
@@ -0,0 +1,17 @@
+#lets say welcome 
+print("Welcome to the bill calculator")
+
+cost = int(input(("What is the total cost? $")))
+
+tip = int(input(("How much tip do you want to give? $")))
+
+people = int(input(("how many people do you want to divide price between? ")))
+
+result = (((cost + tip)/people))
+
+print("$" + str(result))
+
+#This was the Day #2 project from Angela Yu's Udemy course.
+
+#I tried to make it completed with myself ,i had seen the content from Angela then i tried 
+#integar
